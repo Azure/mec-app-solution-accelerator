32c4581a7d2f61e940e5dd89de606d85b33ee14f
javier.perez@turingchallenge.com <javier.perez@turingchallenge.com>
2022-12-05 11:32:34 +0100

    ML Events v1

--- src/ML/ai_inferencer/invoke-sender-frames.py
+++ src/ML/ai_inferencer/invoke-sender-frames.py
@@ -9,11 +9,16 @@ model = torch.hub.load("ultralytics/yolov5", "yolov5s")
 @app.method(name='frames-receiver')
 def framesreceiver(request: InvokeMethodRequest) -> InvokeMethodResponse:
     # model = torch.hub.load("ultralytics/yolov5", "yolov5s")
-    image_list = json.loads(request.text())['image']
-    frame=numpy.array(image_list)
-    main(model,frame)
+    print("in")
+
+    frame = json.loads(request.text())['image']
+    # frame=numpy.array(image_list)
+    detection_threshold=0.7
     # print(request.metadata, flush=True)
     # print(request.text(), flush=True)
+    path='src/detection.avro'
+    main(model,frame,detection_threshold,path)
+
     return InvokeMethodResponse(b'Frame Analyzed', "text/plain; charset=UTF-8")
 
 app.run(50051)
--- src/ML/ai_inferencer/src/inference.py
+++ src/ML/ai_inferencer/src/inference.py
@@ -7,6 +7,11 @@ import os
 import base64
 import cv2
 import numpy as np
+import avro.schema
+from avro_json_serializer import AvroJsonSerializer
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+import io
 
 
 def PublishEvent(pubsub_name: str, topic_name: str, data: str):
@@ -14,21 +19,50 @@ def PublishEvent(pubsub_name: str, topic_name: str, data: str):
         resp = client.publish_event(pubsub_name=pubsub_name, topic_name=topic_name, data=data, data_content_type='application/json')
         print(resp)
 
-def main(model,frame):
+# def main(model,frame):
+def main(model,frame,detection_threshold,path):
+    print(os.listdir())
     print('i')
+    # "Frame": backToBytes.decode()
     backToBytes = base64.standard_b64decode(frame)
+
     img = cv2.imdecode(np.frombuffer(backToBytes, np.uint8), cv2.IMREAD_COLOR)
-
+    print(img)
+    data = { "UrlVideoEncoded": "1.0",
+    "Frame": frame,
+    "EventName": "ObjectDetection",
+    "EventType": "class",
+    "OriginModule": "Ai inference detection",
+    "Information": "Test message",
+    "EveryTime": int(time.time()),
+    "BoundingBoxes": []
+    }
     results = model(img)
-    print(results)
-    detections = []
-    for detection in json.loads(results.pandas().xyxy[0].to_json())["name"].values():
-        detections.append(detection)
-    detections=list(set(detections))
-    data_str = {"information":"Yolo detected: " + ', '.join(detections)}
-    print(json.dumps(data_str))
-    PublishEvent(pubsub_name="pubsub", topic_name="newDetection", data=json.dumps(data_str))
-
+    schema = avro.schema.Parse(open(path, "rb").read())
+    # serializer = AvroJsonSerializer(schema)
+    writer = avro.io.DatumWriter(schema)
+    bytes_writer = io.BytesIO()
+    encoder = avro.io.BinaryEncoder(bytes_writer)
+    detections = json.loads(results.pandas().xyxy[0].to_json())
+    for idx,detection in enumerate(detections["name"].values()):
+        data["BoundingBoxes"]=[]
+        
+        if list(detections["confidence"].values())[idx] > detection_threshold:
+            data["EventType"]=detection
+            xmin=list(detections["xmin"].values())[idx]
+            xmax=list(detections["xmax"].values())[idx]
+            ymin=list(detections["ymin"].values())[idx]
+            ymax=list(detections["ymax"].values())[idx]
+            data["BoundingBoxes"].append({"x": xmin, "y":ymin})
+            data["BoundingBoxes"].append({"x": xmin, "y":ymax})
+            data["BoundingBoxes"].append({"x": xmax, "y":ymin})
+            data["BoundingBoxes"].append({"x": xmax, "y":ymax})
+            # json_str = serializer.to_json(data)
+            writer.write(data, encoder)
+            bbytes = bytes_writer.getvalue()
+            print(bbytes)
+            PublishEvent(pubsub_name="pubsub", topic_name="newDetection", data=bbytes)
+    return
     # results.print()
 
         
@@ -37,5 +71,11 @@ def main(model,frame):
 
 
 if __name__ == '__main__':
-    os.system("python3 invoke-sender-frames.py")
-    main()
+    #os.system("python3 invoke-sender-frames.py")
+    model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True,force_reload=True )
+    img = cv2.imread('coches2.jpg')
+    retval, buffer = cv2.imencode('.jpg', img) 
+    frame_out=base64.b64encode(buffer)
+    path='detection.avro'
+    detection_threshold=0.7
+    main(model, frame_out, detection_threshold,path)
