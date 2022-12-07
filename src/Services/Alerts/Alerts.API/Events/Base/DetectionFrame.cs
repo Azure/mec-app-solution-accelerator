77f311094a84cdd3306fad24ba627643cad7b8cf
Dsaezvil <dsaezvil@everis.com>
2022-12-07 11:31:39 +0100

    Schemas and base events .net added.

---
+++ src/Services/Alerts/Alerts.API/Events/Base/DetectionFrame.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.MecSolutionAccelerator.Services.Alerts.Events.Base
+{
+    public class DetectionFrame
+    {
+        public string Frame { get; set; }
+        public string Type { get; set; }
+        public string UrlVideoEncoded { get; set; }
+        public List<BoundingBox> BoundingBoxes { get; set; }
+    }
+}
