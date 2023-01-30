23e4cc1d2cab2c8be538fbdd8e49c3e90e770527
jaimedeclemente <78213320+jaimedeclemente@users.noreply.github.com>
2023-01-30 09:45:13 -0600

    Adds scroll persistence and automatic refresh switch

--- src/Web/MyFrontEnd/Controllers/AlertController.cs
+++ src/Web/MyFrontEnd/Controllers/AlertController.cs
@@ -16,6 +16,7 @@ namespace MyFrontEnd.Controllers
     public class AlertController : Controller
     {
         public IEnumerable<Alert> Alerts;
+        //public List<Alert> Alerts;
         private readonly DaprClient _daprClient;
 
         public AlertController(DaprClient daprClient)
@@ -49,5 +50,50 @@ namespace MyFrontEnd.Controllers
             ViewData["Alerts"] = Alerts;
             return "New data added";
         }
+
+        //public int count = 0;
+
+        //public ActionResult RefreshPage()
+        //{
+        //    RefreshData();
+        //    IndexModel Model = new IndexModel();
+        //    Model.Alerts = Alerts;
+        //    return View("Alert", Model);
+        //}
+
+        //public string RefreshData()
+        //{
+        //    Console.WriteLine("Here I am!");
+        //    Alerts = new List<Alert>();
+        //    Alerts.Add(new Alert("1", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("2", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("3", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("4", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("5", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("6", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("7", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("8", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("9", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("10", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("11", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("12", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("13", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("14", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("15", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("16", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("17", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("18", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("19", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("20", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("21", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    Alerts.Add(new Alert("22", new Source("Camera 1", "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    for(int i = 0; i<=count; i++)
+        //    {
+        //        Alerts.Add(new Alert((22 + i).ToString(), new Source("Camera "+(i).ToString(), "camera", 124, 125), "", "", "", 75, DateTime.Now, DateTime.Now));
+        //    }
+        //    count = count + 1;
+        //    ViewData["Alerts"] = Alerts;
+        //    return "New data added";
+        //}
     }
 }
