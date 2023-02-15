4723dfe19d32504f6ff0ebcb345cc91763b37bf1
Pablo Ramos Villaplana <pabravil@gmail.com>
2023-02-15 19:20:46 +0100

    Remove Alerts UI SPA project

--- src/Web/alerts-ui/src/components/icons/IconNavBar.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width=".5" stroke="black" class="w-auto h-auto px-3 py-5 hover:bg-selected active:bg-click">
-      <slot></slot>
-    </svg>
-</template>
