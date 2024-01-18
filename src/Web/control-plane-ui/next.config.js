e7b3d5508cfe84bdebdccfb22a8860d8d1eaef02
Carlos Carrillo Boj <carlos.carrillo@plainconcepts.com>
2024-01-17 19:03:24 -0800

    Using standalone deployment configuration

--- src/Web/control-plane-ui/next.config.js
+++ src/Web/control-plane-ui/next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig =  {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
     });
-  
+
     return config;
   },
+  output: 'standalone',
 };
 
 module.exports = nextConfig
