ee601e3f8f5f626023015b79e539241e26f7c3ac
Carlos Carrillo Boj <carlos.carrillo@plainconcepts.com>
2024-02-07 15:55:54 -0800

    Render react player on client only

--- src/Web/control-plane-ui/src/app/camera-dashboard/Camera.tsx
+++ src/Web/control-plane-ui/src/app/camera-dashboard/Camera.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import React from 'react';
-import dynamic from 'next/dynamic';
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
+import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
 
 export type CameraProps = {
   src: string;
@@ -11,9 +10,19 @@ export type CameraProps = {
 export default function Camera({
   src
 }: CameraProps) {
+  const [hasWindow, setHasWindow] = useState(false);
+  const [playerKey, setPlayerKey] = useState(1);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
+
   return (
     <div>
-      <ReactPlayer
+      {hasWindow && <ReactPlayer
+        key={playerKey}
         url={src}
         loop={true}
         controls={true}
@@ -21,7 +30,14 @@ export default function Camera({
         width="100%"
         height="auto"
         playing={true}
-      />
+        onError={(e) => {
+          // Refresh player in 1 second, during camera initialization
+          // We can find 404 until the stream is created/active
+          setTimeout(() => {
+            setPlayerKey(playerKey + 1);
+          }, 1000);
+        }}
+      />}
     </div>
   )
 }
