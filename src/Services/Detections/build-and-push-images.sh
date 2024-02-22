d955c2fdce56d22c151e25bc8793e96e64cf402a
Carlos Carrillo Boj <carlos.carrillo@plainconcepts.com>
2024-02-22 15:24:20 -0800

    Increase framesplitter to 1.8.1

--- src/Services/Detections/build-and-push-images.sh
+++ src/Services/Detections/build-and-push-images.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build and publish framesplitter
-tag="1.8"
+tag="1.8.1"
 image_name="mecsolutionaccelerator/framesplitter:$tag"
 
 docker build --tag=$image_name --file=./frameSplitter/Dockerfile .
