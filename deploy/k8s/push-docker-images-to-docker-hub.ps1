7cd2f9823666196ccaf4a6c8b4efa900d41ab72b
vrubio <virubiotor@gmail.com>
2024-04-03 07:28:11 +0200

    bump alerts-ui image version

--- deploy/k8s/push-docker-images-to-docker-hub.ps1
+++ deploy/k8s/push-docker-images-to-docker-hub.ps1
@@ -1,4 +1,4 @@
-docker push mecsolutionaccelerator/alerts-ui:1.6
+docker push mecsolutionaccelerator/alerts-ui:1.8
 docker push mecsolutionaccelerator/framesplitter:1.6
 docker push mecsolutionaccelerator/aiinferencer:1.6
 docker push mecsolutionaccelerator/alerts-api:1.6
