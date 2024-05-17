a4df8fc6d5deb0435d52096e3355ec0bd7b41e11
PLAINCONCEPTS\vrubio <vrubio@plainconcepts.com>
2024-05-17 09:44:17 +0200

    feat: update ControlPlane API dependencies + bump image version to 2.0

--- src/Services/ControlPlane/API/build-and-push-image.sh
+++ src/Services/ControlPlane/API/build-and-push-image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build and publish control plane ui image
-tag="1.8.1"
+tag="2.0"
 image_name="mecsolutionaccelerator/control-plane-api:$tag"
 
 docker build --tag=$image_name --file=./Dockerfile .
