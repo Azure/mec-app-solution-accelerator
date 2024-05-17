f2816bebd5e53b06617b327cd06ef5fb33e18894
PLAINCONCEPTS\vrubio <vrubio@plainconcepts.com>
2024-05-17 14:53:47 +0200

    feat: improve scripts to show installation info clearly

--- deploy/k8s/deploy-accelerator.ps1
+++ deploy/k8s/deploy-accelerator.ps1
@@ -54,44 +54,66 @@ else {
     Write-Host "Installing Mec-Accelerator with mosquitto MQTT broker"
 }
 
+Write-Host ""
+Write-Host "----------------------------------------------------------------------------------"    
 Write-Host "1. Installing Helm"
 winget install Helm.Helm
 
 if (-not (kubectl get ns | Select-String 'dapr-system')) {
+    Write-Host ""
+    Write-Host "----------------------------------------------------------------------------------"    
     Write-Host "2. Installing Dapr in cluster"
     powershell -Command "iwr -useb https://raw.githubusercontent.com/dapr/cli/master/install/install.ps1 | iex"
     dapr init -k
 }
 
+Write-Host ""
+Write-Host "----------------------------------------------------------------------------------"    
 Write-Host "3. Creating Mec-Accelerator namespace"
 kubectl apply -f ./00-namespace.yaml
 
+Write-Host ""
+Write-Host "----------------------------------------------------------------------------------"    
 Write-Host "4. Installing Akri"
 helm repo add akri-helm-charts https://project-akri.github.io/akri/
 helm repo update
 helm install akri akri-helm-charts/akri -n mec-accelerator --set kubernetesDistro=$kubernetesDistro --set custom.discovery.enabled=true --set custom.discovery.image.repository=mecsolutionaccelerator/akri-camera-discovery-handler --set custom.discovery.image.tag=2.0 --set custom.discovery.name=akri-camera-discovery --set custom.configuration.enabled=true --set custom.configuration.name=akri-camera --set custom.configuration.discoveryHandlerName=camera --set custom.configuration.discoveryDetails.connectionString="mongodb://control-plane-mongodb.mec-accelerator:27017" --set custom.configuration.discoveryDetails.database="ControlPlane" --set custom.configuration.discoveryDetails.collection="Cameras" --set custom.configuration.brokerPod.image.repository=mecsolutionaccelerator/framesplitter --set custom.configuration.brokerPod.image.tag=2.0
 Start-Sleep -Seconds 30
 
+Write-Host ""
+Write-Host "----------------------------------------------------------------------------------"    
 Write-Host "5. Deploying MQTT Broker"
 kubectl apply -f ./"$mqttBroker"/
 
+Write-Host ""
+Write-Host "----------------------------------------------------------------------------------"    
 Write-Host "6. Deploying Mec-Accelerator resources"
 kubectl apply -f ./
 Unblock-File ./deploy-akri-secrets.ps1
 ./deploy-akri-secrets.ps1
 
 if (-not (kubectl get ns | Select-String 'kubernetes-dashboard')) {
+    Write-Host ""
+    Write-Host "----------------------------------------------------------------------------------"    
     Write-Host "7. Deploying Kubernetes dashboards"
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
     kubectl apply -f ./dashboard_auth/dashboard-adminuser.yaml 
     kubectl apply -f ./dashboard_auth/adminuser-cluster-role-binding.yaml
-
+    Write-Host ""
+    Write-Host "----------------------------------------------------------------------------------"    
     Write-Host "Kubernetes dashboards installed."
     Write-Host "Please run 'kubectl proxy' to access the dashboard at http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
     Write-Host "Generate a valid bearer token for the dashboard with 'kubectl -n kubernetes-dashboard create token admin-user --duration=48h --output yaml'"
+    Write-Host "----------------------------------------------------------------------------------"    
 }
 
+Write-Host ""
+Write-Host "----------------------------------------------------------------------------------"    
 Write-Host "Successfully installed MEC-Accelerator!"
 Write-Host ""
 Write-Host "Alerts-UI and Control-Plane-UI services deployed in:"
-kubectl get services -n mec-accelerator -o=custom-columns=NAME:.metadata.name,IP:.status.loadBalancer.ingress[*].ip,PORT:.spec.ports[*].port | Select-String -Pattern alerts-ui[^-], control-plane-ui-service
+Write-Host "----------------------------------------------------------------------------------"
+kubectl get service -n mec-accelerator control-plane-ui-service -o jsonpath='| Control Plane web app URL (control-plane-ui-service pod) | http://{.status.loadBalancer.ingress[*].ip}:{.spec.ports[*].port} |{"\n"}'
+Write-Host "----------------------------------------------------------------------------------"
+kubectl get service -n mec-accelerator alerts-ui -o jsonpath='| Alerts Dashboard web app (alerts-ui pod)                 | http://{.status.loadBalancer.ingress[*].ip}:{.spec.ports[*].port} |{"\n"}'
+Write-Host "----------------------------------------------------------------------------------"
